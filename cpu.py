9e659f7de6fbab6dd476019fe4593bdc2ca6c20f
Kawsar Hussen <kawsarhussen@gmail.com>
2019-07-26 13:09:08 -0400

    Bug fixed and working

--- cpu.py
+++ cpu.py
@@ -1,3 +1,4 @@
+"""CPU functionality."""
 
 import sys
 
@@ -6,53 +7,67 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        self.reg = [0] * 0x08
-        # holds ff= 255 bytes
-        self.ram = [0] * 0xff
-        #Program Counter, address of the currently executing instruction
-        self.pc = 0x00 
-        #Instruction Register, contains a copy of the currently executing instruction
-        self.ir = 0x00
-        #opcodes to 1
-        self.h = 0b00000001 
-        #flag
-        self.fl = 0*00
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.reg[7] = 0xf4
+        self.PC = 0
+        self.MAR = 0
+        self.MDR = 0
+        self.IR = 0
+        self.SP = 7
+        self.FL = 0
+        self.E = 0
+        self.L = 0
+        self.G = 0
+    
+    def ram_read(self, MAR):
+        return self.ram[MAR]
 
-    def ram_read(self, current):
-        return self.ram[current]
-    def ram_write(self, write, current):
-        self.ram[current] = write
+    def ram_write(self, MAR, MDR):
+        self.ram[MAR] = MDR
+
+    def run_push(self, a):
+        self.reg[self.SP] -= 1
+        self.ram[self.reg[self.SP]] = self.reg[a]
+
+    def run_pop(self, a):
+        self.reg[a] = self.ram[self.reg[self.SP]]
+        self.reg[self.SP] += 1
 
     def load(self):
         """Load a program into memory."""
+
         address = 0
-        # For now, we've just hardcoded a program:
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        with open(sys.argv[1]) as f:
+            for line in f:
+                if line[0] != '#' and line != '\n':
+                    self.ram[address] = int(line[0:8], 2)
+                    address += 1
+            f.closed
 
-    def increment_pc(self, op_code):
-        add_to_pc = (op_code >> 6) + 1
-        self.pc += add_to_pc
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
-
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         elif op == "MUL":
             self.reg[reg_a] *= self.reg[reg_b]
 
+            self.reg[reg_a] = self.reg[reg_a] % self.reg[reg_b]
+        elif op == "CMP":
+            if self.reg[reg_a] == self.reg[reg_b]:
+                self.E = 1
+                self.L = 0
+                self.G = 0
+            elif self.reg[reg_a] <= self.reg[reg_b]:
+                self.E = 0
+                self.L = 1
+                self.G = 0
+            else:
+                self.E = 0
+                self.L = 0
+                self.G = 1
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -75,56 +90,62 @@ class CPU:
             print(" %02X" % self.reg[i], end='')
 
         print()
-
+  
     def run(self):
         """Run the CPU."""
+        cpu_run = True
+
         HLT = 0b00000001
-        CMP = 0b10100111 
+        LDI = 0b10000010
+        PRN = 0b01000111
+        PUSH = 0b01000101
+        POP = 0b01000110
+        RET = 0b00010001
+        ADD = 0b10100000
+        MUL = 0b10100010
+        JMP = 0b01010100
+        CMP = 0b10100111
         JEQ = 0b01010101
         JNE = 0b01010110
-        JMP = 0b01010100
-        run_cpu = True
-        
-        while run_cpu:
-            self.ir = self.pc
-            command = self.ram_read(self.pc)
+  
 
-            o1 = self.ram_read(self.pc + 1)
-            o2 = self.ram_read(self.pc + 2)
+        while(cpu_run):
+            operand_a = self.ram_read(self.PC + 1)
+            operand_b = self.ram_read(self.PC + 2)
+            # print(self.PC)
+            if self.ram[self.PC] == HLT:
+                cpu_run == False
+                break
 
-            if self.ram[self.ir] == self.h:
-                run_cpu = False
+            elif self.ram[self.PC] == LDI:
+                self.reg[operand_a] = operand_b
+                self.PC += 3
 
-            elif command == HLT:  # HLT (halt)
-                run_cpu = False
-                sys.exit(1)
+            elif self.ram[self.PC] == PRN:
+                print(self.reg[operand_a])
+                self.PC += 2
 
-            elif command == CMP:
-                if self.reg[o1] == self.reg[o2]:
-                    self.fl = 1
-                elif self.reg[o1] < self.reg[o2]:
-                    self.fl = 4
-                elif self.reg[o1] > self.reg[o2]:
-                    self.fl = 2
-            elif command == JMP:  # jump
-                # get the register address out of the memory
-                reg_address = self.ram_read(self.pc + 1)
-                self.pc = self.registers[reg_address]
 
-            elif command == JEQ:
-                reg_address = self.ram_read(self.pc +1) # getting register address from memory
-                if self.fl == 1:  #check if equal is true
-                    self.pc = self.reg[reg_address]
+            elif self.ram[self.PC] == JMP:
+                self.PC = self.reg[operand_a]
+
+            elif self.ram[self.PC] == CMP:
+                self.alu("CMP", operand_a, operand_b)
+                self.PC += 3
+
+            elif self.ram[self.PC] == JEQ:
+                if self.E == 1:
+                    self.PC = self.reg[operand_a]
                 else:
-                    self.increment_pc(command)
-            elif command == JNE:
-                reg_address = self.ram_read(self.pc + 1)
-                if self.fl != 1:
-                    self.pc = self.reg[reg_address]
+                    self.PC += 2
+
+            elif self.ram[self.PC] == JNE:
+                if self.E == 0:
+                    self.PC = self.reg[operand_a]
                 else:
-                    self.increment_pc(command)
+                    self.PC += 2
 
             else:
-                run_cpu== False
+                cpu_run == False
                 break
                 print(f"You have entered an invalid command: {self.ram[self.PC]}. Program exiting...")
